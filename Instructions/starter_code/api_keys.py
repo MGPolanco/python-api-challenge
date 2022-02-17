19c3a7cedbe88b7835e74d7f715e7b3336519eb0
Mindy Polanco <mindy@Mindys-MacBook-Pro.local>
2022-02-16 19:01:04 -0600

    load_csv

--- Instructions/starter_code/api_keys.py
+++ Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "d0a04936d1070e7063564beca6822b2d"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBnYozggTE3oe9XMRUGcfm9PI7Tf-Rn-Rw"
